4bf5bbdaeaf50e29f0ffe0017be2b9a3cb6afcf9
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-21 00:19:19 -0300

    build dotnet

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -6,7 +6,11 @@ namespace DigitalInnovationOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Console.WriteLine("Hello World!");*/
+            int numeroDeVezes = 5;
+            for (int i=0; i < numeroDeVezes; i++){
+                Console.WriteLine($"Dem-vindo ao curso de dotnet {i}");
+            }
         }
     }
 }
