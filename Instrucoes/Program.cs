469579d9974d4c6cc028efb9ccf9a358c43893da
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-22 12:18:30 -0300

    aulas dotnet debugando o código pilhas.

---
+++ Instrucoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Instrucoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
