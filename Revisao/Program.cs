7ee8779a43ba94d10a24e1d611777c66a53e6ea7
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-23 00:41:41 -0300

    criando menu e método.

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,11 +6,40 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //Todo: Adicionar aluno 
+                        break;
+
+
+                    case "2":
+                        //Todo: Listar alunos 
+                        break;
+
+                    case "3":
+                        //Todo: Calcular Média 
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        private static string ObterOpcaoUsuario()
+        {
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar os alunos");
             Console.WriteLine("3 - Calculdar média geral");
+            Console.WriteLine("X - Sair da Aplicação");
+
             string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
         }
     }
 }
