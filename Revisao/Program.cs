ee40ebf0dbecc14073d220004c42ae957d738411
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-23 00:29:37 -0300

    Criando projeto de revisão.

---
+++ Revisao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar os alunos");
+            Console.WriteLine("3 - Calculdar média geral");
+            string opcaoUsuario = Console.ReadLine();
+        }
+    }
+}
