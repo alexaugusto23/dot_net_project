64e1e0d8519ad5cf40e72c3890114771d9de894b
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-24 14:55:51 -0300

    criando build.

--- QuantaMandioca/Program.cs
+++ QuantaMandioca/Program.cs
@@ -13,7 +13,12 @@ namespace QuantaMandioca
             int marina = 1000 * Int32.Parse(Console.ReadLine());
             int iara = 150 * Int32.Parse(Console.ReadLine());
             int marlene = 225;
-            int total = chico + bento+bernardo+marina+iara+marlene; // Digite aqui o calculo total
+            int total = chico + 
+                        bento +
+                        bernardo +
+                        marina +
+                        iara +
+                        marlene; // Digite aqui o calculo total
 
             Console.WriteLine(total);
 
