ea1f06b81ff5a56eabf5427e5760872dfff22a9f
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-28 20:46:39 -0300

    desafios dotnet e C#

---
+++ Quadradoeaocubo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Quadradoeaocubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+                Console.Write(i*i + " ");
+                Console.Write(i*i*i);
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
