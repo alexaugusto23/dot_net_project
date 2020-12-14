88a5cd13d3461c495ff1a22df219c1be7739a163
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-12-14 15:28:59 -0300

    desafios completos.

--- Rodizio_de_cavalos_e_carruagens/Program.cs
+++ Rodizio_de_cavalos_e_carruagens/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Linq;
 
 namespace Rodizio_de_cavalos_e_carruagens
 {
     class Program
     {
-        static void Main(string[] args)
+            static void Main(string[] args)
+            {
+                int quantidadeTestes = int.Parse(Console.ReadLine());
+                string[] Placas = new string[quantidadeTestes];
+
+                for (int i = 0; i < quantidadeTestes; i++) 
+                {
+                    Placas[i] = Console.ReadLine();
+                }
+
+                for (int i = 0; i < quantidadeTestes; i++) {
+                if (ValidarPlaca(Placas[i]) == true) {
+                    int dig = PegarUltimo(Placas[i]);
+                    string rod = CalcularRod(dig);
+                    Console.WriteLine(rod);
+                } else {
+                    Console.WriteLine("FALHA");
+                }
+            }
+        }
+
+
+        public static bool ValidarPlaca(string placa) 
+        { 
+            string[] campos = placa.Split("-");
+            if (campos.Length == 2) {
+                string letras = campos[0];
+                string nums = campos[1];
+                if (letras.Length == 3 && nums.Length == 4) 
+                {
+                    if (letras.All(char.IsLetter) && letras.All(char.IsUpper) && nums.All(char.IsDigit)) 
+                    {
+                        return true;
+                    } else {return false;}
+                } else {return false;}
+            } else {return false;}
+        }
+
+
+        public static int PegarUltimo(string placa) 
         {
-            Console.WriteLine("Hello World!");
+            return int.Parse(placa.Substring(placa.Length -1));
+        }
+
+
+        public static string CalcularRod(int digito) 
+        {
+            if (digito == 1 || digito == 2) 
+                {
+                    return "SEGUNDA";
+                } 
+            else if (digito == 3 || digito == 4) 
+                {
+                    return "TERCA";
+                } 
+            else if (digito == 5 || digito == 6) 
+                {
+                    return "QUARTA";
+                } 
+            else if (digito == 7 || digito == 8) 
+                {
+                    return "QUINTA";
+                } 
+            else if (digito == 9 || digito == 0) 
+                {
+                    return "SEXTA";
+                } 
+            else 
+                {
+                    return "FALHA";
+                }
         }
     }
+    
 }
