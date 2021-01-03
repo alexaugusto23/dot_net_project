13b41056ab90d1114a5c30ba2eb8ec9fe949c212
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-03 00:08:23 -0300

    novos projetos.

---
+++ AlbumdaCopa/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AlbumdaCopa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+            int totalDeFigurinhas = 0;
+            int [] albumDeFigurinha = new int[numeroDeFigurinhas];
+            int countfig = 0;
+
+            for(int i = 0; i < albumDeFigurinha.Length; i++)
+            {
+                int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+                albumDeFigurinha[i] = numeroDeFigurinhasCompradas;
+                if(albumDeFigurinha[i] > 0)
+                {countfig++;}
+
+                //Console.WriteLine(albumDeFigurinha[i]);
+            }
+
+            for (int i = 0; i < countfig; i++)
+            {
+                for( int j=1; j < countfig; j++)
+                {
+                    //Console.Write(albumDeFigurinha[i] + " ");
+                    //Console.WriteLine(albumDeFigurinha[j]);
+                    if(albumDeFigurinha[0] == albumDeFigurinha[j])
+                    {
+                        countfig -= 1;
+                    } 
+                }
+
+            }
+            //Console.WriteLine($"countfig: {countfig}");
+
+            totalDeFigurinhas = numeroDeFigurinhas;
+            totalDeFigurinhas -= countfig;
+            //Console.WriteLine($"Total de figurinhas: {totalDeFigurinhas - countfig}");
+            Console.WriteLine(totalDeFigurinhas);
+        }
+    }
+}
---
+++ Calculodeviagem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculodeviagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                string [] entrada = Console.ReadLine().Split(" ");
+                float qtd = Int32.Parse(entrada[0]) * Int32.Parse(entrada[1]);
+                float valor = qtd/12;
+                Console.WriteLine(valor.ToString("N3"));
+        }
+    }
+}
